d80a96c5b40719dbe453467ff15bb52bcc985861
2Duffman <96424947+2Duffman@users.noreply.github.com>
2022-11-22 15:42:30 +0100

    refactor: change sparse matrices

--- jl_programs/puzzlegraph2.jl
+++ jl_programs/puzzlegraph2.jl
@@ -4,13 +4,13 @@ start42 = [Int8[0,0,0,1,0,2,3,4,0,5,6,7,8,9,10,11]]
 start43 = [Int8[0,0,0,1,0,2,3,4,0,0,0,0,0,0,0,5]]
 start54 = [Int8[1,2,3,0,4,0,0,0,5,0,0,0,0,0,0,0,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 
-#sparce starting configurations
-sparce_start32 = [Int8[0,1,2,4]]
-sparce_start43 = [Int8[0, 1, 2, 4, 8]]
-sparce_start54 = [Int8[0, 1, 2, 4, 8, 16]]
-sparce_start42_4tiles= [Int8[11,1,7,4]]
-sparce_start42_5tiles= [Int8[0,1,2,4,8]]
-sparce_start42_6tiles= [Int8[0,1,2,4,8,15]]
+#sparse starting configurations
+sparse_start32 = [Int8[0,1,2,4]]
+sparse_start43 = [Int8[0, 1, 2, 4, 8]]
+sparse_start54 = [Int8[0, 1, 2, 4, 8, 16]]
+sparse_start42_4tiles= [Int8[11,1,7,4]]
+sparse_start42_5tiles= [Int8[0,1,2,4,8]]
+sparse_start42_6tiles= [Int8[0,1,2,4,8,15]]
 
 #facecollections
 facecollection32 = [[[1, 2, 3], [1, 4, 5], [2, 4, 6]], [[0, 2, 3], [0, 4, 5], [3, 5, 7]], [[0, 1, 3], [3, 6, 7], [0, 4, 6]], [[0, 1, 2], [2, 6, 7], [1, 5, 7]], [[5, 6, 7], [0, 1, 5], [0, 2, 6]], [[4, 6, 7], [0, 1, 4], [1, 3, 7]], [[4, 5, 7], [2, 3, 7], [0, 2, 4]], [[4, 5, 6], [2, 3, 6], [1, 3, 5]]]
